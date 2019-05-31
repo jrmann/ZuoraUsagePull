4b1fd40a6e4ec5d12bd818b0c2b7c70669e64956
jrmann <49927613+jrmann@users.noreply.github.com>
2019-05-30 19:27:41 -0700

    Add files via upload

--- ZuoraQuery.py
+++ ZuoraQuery.py
@@ -52,3 +52,8 @@ for record in zuora.query_all(
         i['account_number'] = record['AccountNumber']
         i['account_name'] = record['Name']
         usage_records.append(i)
+
+        df = pd.DataFrame(usage_records)
+df['usage_month'] = df['StartDateTime'].astype('datetime64[M]')
+grouped = df.groupby(['account_name','account_number','usage_month','UOM']).Quantity.sum().reset_index()
+df.head(10)
